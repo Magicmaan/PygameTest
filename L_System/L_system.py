7c14accfebb4c758028b4a90c2d0563e37976a36
MagicMaan <38400400+Magicmaan@users.noreply.github.com>
2024-07-15 01:47:31 +0100

    ugh

--- L_System/L_system.py
+++ L_System/L_system.py
@@ -6,32 +6,36 @@ tturtle.screen.title("L-System Derivation")
 tturtle.speed(0)  # adjust as needed (0 = fastest)
 turtle_screen = turtle.Screen()  # create graphics window
 turtle_screen.screensize(1500, 1500)
+tturtle.hideturtle()
 
 
-def function_Fwd(settings):
-    tturtle.forward(settings["seg_length"])
+def function_Fwd(turtle,settings):
+    turtle.forward(settings["seg_length"])
 
-def function_FwdNodraw(settings):
-    tturtle.pu()  # pen up - not drawing
-    tturtle.forward(settings["seg_length"])
-    tturtle.pd()
+def function_FwdNodraw(turtle,settings):
+    turtle.penup()  # pen up - not drawing
+    turtle.forward(settings["seg_length"])
 
-def function_rotRight(settings):
-    tturtle.right(settings["seg_length"])
+def function_rotRight(turtle,settings):
+    turtle.right(settings["angle"])
 
-def function_rotLeft(settings):
-    tturtle.left(settings["angle"])
+def function_rotLeft(turtle,settings):
+    turtle.left(settings["angle"])
 
-def function_widthIncrease(settings):
-    currentWidth = tturtle.pensize()
-    tturtle.pensize(currentWidth + settings["widthStep"])
+def function_widthIncrease(turtle,settings):
+    currentWidth = turtle.pensize()
+    turtle.pensize(currentWidth + settings["widthStep"])
 
-def function_widthDecrease(settings):
-    currentWidth = tturtle.pensize()
-    tturtle.pensize(min(0,currentWidth - settings["widthStep"]))
-
+def function_widthDecrease(turtle,settings):
+    currentWidth = turtle.pensize()
+    turtle.pensize(min(0,currentWidth - settings["widthStep"]))
 def function_flower():
     pass
+def function_setPosRot(turtle,position,angle=False):
+    turtle.pu()
+    turtle.goto(position)
+    if angle:
+        turtle.setheading(angle)
 
 
 #Base instruction set bindings for functions of turtle
@@ -43,23 +47,26 @@ commandList = {
     "!" :           function_widthIncrease, #Decrease branch Width
     "@" :           function_widthDecrease, #Increase branch Width
     "%" :           function_flower,        #flower / bud
+    "£" :           function_setPosRot,
 }
 
 #Rule Set
 #defines shorthand for commands
 #loops can be formed with [ ] which allow to branch off and then return
 rules = {
-    "G" : "-FF[+++FFF---]FF"
+    "G" : "FFFF[+ffffFFF]FFFF+"
 }
 
 
 class LSys():
-    def __init__(self,axiom,mode=0):
+    def __init__(self,turtle,axiom,mode=0):
+        self.turtle = turtle
+
         self.settings = {
             "seg_length" : 10,
-            "angle" : 45,
+            "angle" : 90,
             "widthStep" : 1,
-            "depth" : 8 ,
+            "depth" : 1 ,
             "axiom" : axiom
         }
         #decompress input axiom to instructions
@@ -68,54 +75,71 @@ class LSys():
         #draw it
         self._drawBasic(0)
 
-
-
-
     def _decompressAxiom(self):
         #Decompress a rule shorthand into instructionset for turtle / draw
-        instructionset = "G"
-        instructions = []
-
+        instructionset,currentString = "G","G"
+        instructions = ""
+        nextAxiom = instructionset
         #how many times to repeat pattern recursively
         for loop in range(self.settings["depth"]):
             #get last Axiom
-            nextAxiom = instructionset[-1]
             #find corresponding instructionset for the Rule and unpack
-            nextSequence = [self._decompressRule(char) for char in nextAxiom]
-            instructions.append("".join(nextSequence))
+            nextSequence = self._decompressRule(nextAxiom)
+            instructions += nextSequence
         
         return instructions
     
     def _decompressRule(self,rule):
         #if its a rule, return the rules instruction
-        if rule in rules.keys():
-            return rules[rule]
-        #else return itself i.e. its already an instruction
-        return rule
+        return rules.get(rule)
+
     
     def _drawBasic(self,string):
+        inloop = True
+        stack = []
         #for command in the turtles instructions
         for cmd in self.instructions:
+            turtle.pd()
             print(cmd)
 
+            if cmd in commandList.keys():
+                commandList[cmd](self.turtle,self.settings)
+            else:
+                print(cmd + " Not Found -------------------")
+                if cmd == "[":
+                    # Save the current state
+                    stack.append((turtle.position(), turtle.heading()))
+                if cmd == "]":
+                    # Restore the last saved state
+                    turtle.pu()  # pen up - not drawing
+                    position, heading = stack.pop()
+                    turtle.goto(position)
+                    turtle.setheading(heading)
+                '''elif False:
+                    elif cmd == "F":
+                        turtle.forward(self.settings["seg_length"])
+                    elif cmd == "+":
+                        turtle.right(self.settings["angle"])
+                    elif cmd == "-":
+                        turtle.left(self.settings["angle"])    '''    
+            '''
             #start a closed loop
             # 1,2, [4,3,2,1], 3,4
             if cmd == "[":
-                pos = turtle.pos()
+                stack.append((turtle.position(), turtle.heading()))
             elif cmd == "]":
-                #backtrack turtle to position before loop
-                turtle.pu()
-                turtle.goto(pos)
-                turtle.pd()
-                pos = turtle.pos()
-                print(" ")
+                turtle.pu()  # pen up - not drawing
+                position, heading = stack.pop()
+                turtle.goto(position)
+                turtle.setheading(heading)
+                
 
             #if corresponding command in instructionset, do its function
             #F = function_Fwd etc
             if cmd in commandList.keys():
-                commandList[cmd](self.settings)
+                commandList[cmd](self.turtle,self.settings)
             else:
-                print("COMMAND: " + cmd + " invalid")
+                print("COMMAND: " + cmd + " invalid ----------")'''
 
 
 
@@ -124,7 +148,7 @@ class LSys():
 
 
 #input ruleset, creates instruction path for a turtle and draws it
-test = LSys("G",0)
+test = LSys(tturtle,"G",0)
 
 #test._drawBasic("--FF+FF+F-FF!!FFFF!!FFFF---FFFFFFfffFFFFF")
 
